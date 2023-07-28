930e21f84ce1c714beed8c32750f1aa659e398d1
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-28 18:58:33 +0530

    changes made

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -1,11 +1,12 @@
 package com.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Component
 public class OrderService {
 
 
